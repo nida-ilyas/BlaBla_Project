e11ce4d15e3a7d34729875c34cef33ee5dc83768
nida-ilyas <nida.ilyas@student.pxl.be>
2016-05-10 16:30:17 +0200

    FIXY

--- ons_project/application/models/ticket_model.php
+++ ons_project/application/models/ticket_model.php
@@ -8,56 +8,52 @@
  */
 class ticket_model
 {
-    var $ticketID ="";
-    var $aanmaker ="";
-    var $onderwerp="";
-    var $prioriteit="";
-    var $type="";
-    var $campusID ="";
-    var $blokID="";
-    var $lokaalNummer="";
-    var $datum="";
-    var $omschrijving="";
-    var $bijlage="";
+    var $ticketID = "";
+    var $aanmaker = "";
+    var $onderwerp = "";
+    var $prioriteit = "";
+    var $type = "";
+    var $campusID = "";
+    var $blokID = "";
+    var $lokaalNummer = "";
+    var $datum = "";
+    var $omschrijving = "";
+    var $bijlage = "";
     var $herstellingDatum;
-    var $deadline="";
-    var $hersteller="";
-    var $status="";
+    var $deadline = "";
+    var $hersteller = "";
+    var $status = "";
 
     /*
     function __construct()
     {
         parent::__construct();
     }
-<<<<<<< HEAD
-    **/
-    /**
-=======
-    */
+*/
 
 
->>>>>>> origin/master
     function getAll()
     {
-        $query = $this -> db ->get('tickets');
-        return $query -> result();
-        return $query -> result();
-        return $query -> result();
+        $query = $this->db->get('tickets');
+        return $query->result();
+        return $query->result();
+        return $query->result();
     }
-    function getList(){
-        $query = $this -> db ->get('tickets');
+
+    function getList()
+    {
+        $query = $this->db->get('tickets');
         return $query->result_array();
-        /**return $query = $this->db->get('tickets');
+        /**return $query = $this->db->get('tickets');**/
     }
-     **/
+
     /**
-    function get_content(){
-        $this->db->select('omschrijving');
-        $this->db->from('tickets');
-        $query = $this->db->get();
-
-        return $result = $query->result();
-    }
+     * function get_content(){
+     * $this->db->select('omschrijving');
+     * $this->db->from('tickets');
+     * $query = $this->db->get();
+     *
+     * return $result = $query->result();
+     * }
      * **/
-
 }
